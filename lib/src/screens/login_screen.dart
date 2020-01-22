9e3d0ce094b2927542b56c65d7d0791f82e0a5cd
Robin <daorob@gmail.com>
2020-01-21 19:41:25 -0600

    Add emailField|passwordField|Container|submitButton widgets to the LoginScreen.

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -4,8 +4,41 @@ class LoginScreen extends StatelessWidget {
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
-      child: Column(children: [],
+      child: Column(
+        children: [
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton(),
+        ],
       ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
     );
   }
 }
